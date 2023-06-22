afa129504e1af041b8b9200ba72f83f3b7601534
Amir JCS <amirmahdyjdx@gmail.com>
2023-06-22 19:21:00 +0330

    style(Builder): remove white spaces

--- Builder/Builder.cs
+++ Builder/Builder.cs
@@ -105,6 +105,16 @@ public class PetB_Builder : IPetBuilder
 
 #endregion
 
+/*
+This is a class that is used to construct an object using the Builder interface.
+So far, we have understood the UML diagram and the definition with its purpose. Now, it is time to look into the actual code to get a better understanding. So now, let’s see this with an example.
+In the example shown below, we will be seeing how we can use this pattern to construct different parts of a pet.
+
+این کلاسی است که برای ساخت یک شی با استفاده از اینترفیس بیلدر استفاده می شود.
+تاکنون نمودار UML و تعریف آن را با هدف آن درک کرده ایم. اکنون زمان آن رسیده است که کد واقعی را بررسی کنیم تا درک بهتری داشته باشیم. خب حالا بیایید این را با یک مثال ببینیم.
+در مثال زیر، خواهیم دید که چگونه می توانیم از این الگو برای ساختن قسمت های مختلف حیوان خانگی استفاده کنیم.
+ 
+ */
 public class PetCreator
 {
     private IPetBuilder _PetBuilder;
